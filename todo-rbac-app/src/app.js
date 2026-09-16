50ea07706415af43f0a6124836335a0261f6858a
tusharTalan1 <tushartalan10@gmail.com>
2026-09-16 21:45:25 +0530

    add 4 routes to the CRUD functions

--- todo-rbac-app/src/app.js
+++ todo-rbac-app/src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const todoRoutes = require("./routes/todoRoutes");
 const app = express();
 app.use(express.json());
+app.use("/todos", todoRoutes);
 module.exports = app;
