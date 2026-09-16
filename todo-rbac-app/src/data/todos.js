623ad20f4c6fb08b70803831ccdb54b07555ef3c
tusharTalan1 <tushartalan10@gmail.com>
2026-09-16 20:02:21 +0530

    add user data and list data

--- todo-rbac-app/src/data/todos.js
+++ todo-rbac-app/src/data/todos.js
@@ -1,1 +1,8 @@
 
+const todos = [
+  {id:1, title:"Learn Node", done:false, owner:2 },
+  {id:2, title:"Finish Project", done:false, owner:3 },
+  {id:3, title:"Build app", done:true, owner:2 },
+];
+
+module.exports = todos;
--- todo-rbac-app/src/data/users.js
+++ todo-rbac-app/src/data/users.js
@@ -1,1 +1,8 @@
 
+const users =[
+  {id:1, name:"Tushar", role:"admin" },
+  {id:2, name:"Rahul", role:"user" },
+  {id:3, name:"Ravi", role:"user" },
+];
+
+module.exports = users;
